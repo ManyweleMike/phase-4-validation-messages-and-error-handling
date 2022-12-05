89ccb4080fdf13d85e07d783bd9362400bf3b4da
ManyweleMike <mugendimike80@gmail.com>
2022-12-05 03:14:32 +0300

    Done

--- client/src/components/MovieForm.js
+++ client/src/components/MovieForm.js
@@ -22,11 +22,14 @@ function MovieForm() {
         "Content-Type": "application/json",
       },
       body: JSON.stringify(formData),
+    }).then((response) => {
+      if (response.ok) {
+        response.json().then((newMovie) => console.log(newMovie))
+      } else {
+        response.json().then((errorData) => setErrors(errorData.errors))
+      }
     })
-      .then((response) => response.json())
-      .then((newMovie) => console.log(newMovie));
   }
-
   function handleChange(e) {
     const value =
       e.target.type === "checkbox" ? e.target.checked : e.target.value;
